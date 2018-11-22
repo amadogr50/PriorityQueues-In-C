b02221c9e8b13d162ba9847b1658d716a202ac47
Omartian <omartian21@gmail.com>
2018-11-22 01:52:52 -0600

    Update priorityqueue.c

--- priorityqueue.c
+++ priorityqueue.c
@@ -232,5 +232,70 @@ Type priorityqueue_poll(PriorityQueue pq)
 
   return data;
 }
+  List list_create(void (*destructor)(Type))
+  {
+  }
+
+  void list_destroy(List)
+  {
+  }
+
+  int  list_size(List)
+  {
+  }
+
+  void list_add(List, Type)
+  {
+  }
+
+  Type list_get(List, int p)
+  {
+  }
+
+  void list_set(List, Type, int p)
+  {
+  }
+
+  Type list_remove(List, int p)
+  {
+  }
+
+  Graph graph_create(PrintFunc printer, DestroyFunc destructor)
+  {
+  }
+
+  void graph_destroy(Graph g)
+  {
+  }
+
+  void graph_addVertex(Graph g, Type u)
+  {
+  }
+
+  void graph_deleteVertex(Graph g, Type v)
+  {
+  }
+
+  void graph_addEdge(Graph g, Type u, Type v, double weight);
+  {
+  }
+
+  void graph_deleteEdge(Graph g, Type u, Type v)
+  {
+  }
+
+
+  void BFS(Graph g, Type start)
+  {
+  }
+
+  void DFS(Graph g)
+  {
+  }
+  void dijkstra(Graph g, Type start)
+  {
+  }
+
+}
 
 
